c0640e5d78d801b65a5ace859a935da784caed25
Limbureuben <limbureuben4@gmail.com>
2025-07-11 20:22:50 +0300

    changes

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function store (Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'discription' => 'required|string',
+            'price' => 'required|string|min:0',
+            'discount' => 'nullable|numeric|min:0|max:100',
+            'stock' => 'required|integer|min:0',
+            'image' => 'nullable|url'
+        ]);
+
+        $product = Product::create($validated);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Product created successfully',
+            'product' => $product
+        ], 201);
+    }
+
+    public function index()
+    {
+        $product = Product::all();
+
+        return response()->json([
+            'success' => true,
+            'products' => $products
+        ]);
+    }
+
+}
