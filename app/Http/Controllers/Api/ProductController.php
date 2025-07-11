4f6619583197f1754d0f8accab54f8445842d7ce
Limbureuben <limbureuben4@gmail.com>
2025-07-11 20:40:45 +0300

    changes

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
-
+namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Http\Controllers\Controller;
 
 class ProductController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UsersController;
+use App\Http\Controller\Api\ProductController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -10,3 +11,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/users', [UsersController::class, 'index']);
 Route::delete('/users/{id}', [UsersController::class, 'destroy']);
 // Route::middleware('auth:sanctum')->delete('/users/{id}', [UsersController::class, 'destroy']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products', [ProductController::class, 'index']);
