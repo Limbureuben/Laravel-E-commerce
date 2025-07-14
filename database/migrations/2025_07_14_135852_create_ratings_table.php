5bc96ee2db6332f9e9cea5633b07626f54eea6ae
Limbureuben <limbureuben4@gmail.com>
2025-07-14 17:07:22 +0300

    changes

--- database/migrations/2025_07_14_135852_create_ratings_table.php
+++ database/migrations/2025_07_14_135852_create_ratings_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->unsignedTinyInteger('rating');
             $table->timestamps();
+
+            $table->unique(['user_id', 'product_id']);
         });
     }
 
