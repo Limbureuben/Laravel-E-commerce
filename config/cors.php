c20e60982b69125bbd48587ef1b18f04bcfcd5c4
Limbureuben <limbureuben4@gmail.com>
2025-07-22 14:37:23 +0300

    changes

--- config/cors.php
+++ config/cors.php
@@ -8,7 +8,7 @@ return [
 
     // 'allowed_origins' => ['http://localhost:5173'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174'],
     'allowed_origins_patterns' => ['/^https:\/\/.*\.ngrok-free\.app$/'],
 
 
